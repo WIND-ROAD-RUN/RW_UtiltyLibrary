92ca3ffbb80025c26eac018c805019541a22067e
风 路 <3461571786@qq.com>
2025-02-05 21:09:13 +0800

    将OrganizeStructure的pugixml实现测试用移到storeStrategy

--- RW/inf/oso/test/src/oso_StorageStrategy_xml_t.cpp
+++ RW/inf/oso/test/src/oso_StorageStrategy_xml_t.cpp
@@ -2,34 +2,50 @@
 
 namespace oso_StorageStraegy_xml
 {
-    TEST(StorageStrategy_xml, test)
-    {
-        ObjectStoreAssembly sampleAssembly;
-        sampleAssembly.setName("test");
+    TEST_F(StorageStrategy_xmlTest, checkStringFormat_getStringAssembly) {
+        auto str = rw::oso::StorageStrategy_Xml::getString(std::make_shared<ObjectStoreAssembly>(_OsoOrganizeStructureDataXmlStoreFormatAssemblySampleData));
+        str = removeNewlinesAndTabs(str);
+        auto standardString = _assemblyStandardString;
 
-        ObjectStoreItem item1;
-        item1.setName("item1");
-        item1.setValueFromInt(1);
-        sampleAssembly.addItem(item1);
-
-        ObjectStoreItem item2;
-        item2.setName("item2");
-        item2.setValueFromDouble(2.0);
-        sampleAssembly.addItem(item2);
-
-        ObjectStoreItem item3;
-        item3.setName("item3");
-        item3.setValueFromBool(true);
-        sampleAssembly.addItem(item3);
-
-        StorageStrategy_Xml testObj;
-        testObj.save(sampleAssembly,R"(C:\Users\34615\Desktop\1\test.xml)");
+        EXPECT_EQ(str, standardString);
     }
 
-    TEST(StorageStrategy_xml, test2)
-    {
-        StorageStrategy_Xml testObj;
-        auto result = testObj.load(R"(C:\Users\34615\Desktop\1\test.xml)");
-        EXPECT_TRUE(result);
-    }   
+
+    TEST_F(StorageStrategy_xmlTest, getStoreAssemblyFromString) {
+        auto target = rw::oso::StorageStrategy_Xml::getStoreAssemblyFromString(_assemblyStandardString);
+
+        auto result = (*target) == ObjectStoreAssembly(_OsoOrganizeStructureDataXmlStoreFormatAssemblySampleData);
+        EXPECT_EQ(result, true);
+    }
+
+    TEST_F(StorageStrategy_xmlTest, getStoreItemFromString) {
+        auto target = rw::oso::StorageStrategy_Xml::getStoreItemFromString(_itemStandardString);
+
+        auto result = (*target) == ObjectStoreItem(_OsoOrganizeStructureDataXmlStoreFormatItemSampleData);
+        EXPECT_EQ(result, true);
+    }
+
+
+    TEST_F(StorageStrategy_xmlTest, xmlCore_getStringAssembly) {
+        auto str = testObj.getString(std::make_shared<ObjectStoreAssembly>(_OsoOrganizeStructureDataXmlStoreFormatAssemblySampleData));
+        str = removeNewlinesAndTabs(str);
+        auto standardString = _assemblyStandardString;
+
+        EXPECT_EQ(str, standardString);
+    }
+
+
+    TEST_F(StorageStrategy_xmlTest, xmlCore_getStoreAssemblyFromString) {
+        auto target = rw::oso::StorageStrategy_Xml::getStoreAssemblyFromString(_assemblyStandardString);
+
+        auto result = (*target) == ObjectStoreAssembly(_OsoOrganizeStructureDataXmlStoreFormatAssemblySampleData);
+        EXPECT_EQ(result, true);
+    }
+
+    TEST_F(StorageStrategy_xmlTest, xmlCore_getStoreItemFromString) {
+        auto target = rw::oso::StorageStrategy_Xml::getStoreItemFromString(_itemStandardString);
+
+        auto result = (*target) == ObjectStoreItem(_OsoOrganizeStructureDataXmlStoreFormatItemSampleData);
+        EXPECT_EQ(result, true);
+    }
 }
